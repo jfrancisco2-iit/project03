0fcb6790e0c20e69f9d728be34c9af967fa62b2c
Jordan Francisco <jfrancisco2@hawk.iit.edu>
2022-11-22 23:44:54 -0600

    added marker

---
+++ js/athensmap.js
@@ -0,0 +1,30 @@
+function init(){
+  //alert ('it works');
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(39.327392,-82.101204)
+  var mapOptions = {
+    center:myLocation,
+	zoom:12,
+	mapTypeId: google.maps.MapTypeId.SATELLITE,
+	mapTypeControlOptions: {
+	  postion: google.maps.ControlPosition.BOTTOM_Center
+	}
+  };
+
+  var myMap = new google.maps.Map(el, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLocation,
+	map: myMap,
+	animation: google.maps.Animation.BOUNCE,
+	icon: 'paw.jpg;
+  
+}
+
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), 
+    {center: {lat: 39.327, lng: -82.101}, 
+	zoom: 10});
+}
+
+google.maps.event.addDomListener(window, 'load', init);
